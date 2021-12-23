54ba59a90dfb15b60e85f38c1a668731d5d5caf4
Quy Khang Huynh <lyhoangkhang831@gmail.com>
2021-12-23 14:58:50 +0700

    upload source code

---
+++ js/labels.js
@@ -0,0 +1,371 @@
+var labels={
+	//Nav
+	"MENU_HOME":{
+		"vi-VN":"TRANG CHỦ",
+		"en-US":"HOME",
+		},
+	"MENU_ABOUT_US":{
+		"vi-VN":"GIỚI THIỆU",
+		"en-US":"ABOUT US",
+		},
+	"MENU_PRODUCTS":{
+		"vi-VN":"CÁC SẢN PHẨM",
+		"en-US":"PRODUCTS",
+		},
+	"MENU_CONTACT":{
+		"vi-VN":"LIÊN HỆ",
+		"en-US":"CONTACT",
+		},
+    "MENU_SEARCH":{
+		"vi-VN":"Tìm",
+		"en-US":"Search",
+		},
+    // DropDown
+    "MENU_PRODUCTS_SEED":{
+		"vi-VN":"Hạt Giống",
+		"en-US":"Seeds",
+		},
+    "MENU_PRODUCTS_FERTILIZER":{
+		"vi-VN":"Phân Bón",
+		"en-US":"Fertilizer",
+		},
+    "MENU_PRODUCTS_TOOLS":{
+		"vi-VN":"Nông Cụ",
+		"en-US":"Agricultural Tools",
+		},
+    "MENU_PRODUCTS_IOT":{
+        "vi-VN":"Thiết bị IoT",
+        "en-US":"IoT Equipments",
+        },
+    // InforAccount
+    "INFOR_LOGOUT":{
+		"vi-VN":"Đăng xuất",
+		"en-US":"Log out",
+		},
+    "INFOR_PROFILE":{
+		"vi-VN":"Thông tin cá nhân",
+		"en-US":"My profile",
+		},
+    "INFOR_BOUGHT":{
+		"vi-VN":"Sản phẩm đã mua",
+		"en-US":"Purchased products",
+		},
+    "INFOR_VIEWED":{
+		"vi-VN":"Sản phẩm đã xem",
+		"en-US":"Viewed products",
+		},
+    "INFOR_FAVOR":{
+		"vi-VN":"Sản phẩm yêu thích",
+		"en-US":"Favorite products",
+		},
+    "INFOR_SETTING":{
+		"vi-VN":"Cài đặt",
+		"en-US":"Setting",
+		},
+    "INFOR_LOGIN":{
+		"vi-VN":"ĐĂNG NHẬP NGAY!",
+		"en-US":"LOGIN NOW!",
+		},
+    "INFOR_INVITE":{
+		"vi-VN":"Bạn chưa có tài khoản<br>Đăng ký ngay với chúng tôi! ",
+		"en-US":"Don't have an account?<br>Sign up now with us!",
+		},
+    "INFOR_SIGNUP":{
+		"vi-VN":"ĐĂNG KÝ",
+		"en-US":"SIGN UP",
+		},
+	//HEADING - slide1
+	"CONTENTS_HEAD":{
+		"vi-VN":"Chào mừng đến với",
+		"en-US":"Welcome to",
+		},
+    "CONTENTS_SLOGAN":{
+        "vi-VN":"SỰ LỰA CHỌN CỦA MỌI NHÀ",
+        "en-US":"CHOICE FOR EVERY HOME",
+        },
+	"CONTENTS_DETAIL_TEXTSTART":{
+		"vi-VN":"Trên mỗi bước đi của chúng ta với thiên nhiên, Ta nhận được nhiều hơn với những gì chúng ta tìm kiếm.",
+		"en-US":"On every step we take with nature, we get more than we seek."
+	},
+    "CONTENTS_BTN_TEXTSTART":{
+		"vi-VN":"Đăng ký ngay hôm nay",
+		"en-US":"Sign up today",
+		},
+    // HEADING - slide2
+	"CONTENTS_HEAD2":{
+		"vi-VN":"Chào mừng đến với",
+		"en-US":"Welcome to",
+		},
+    "CONTENTS_SLOGAN2":{
+        "vi-VN":"SỰ LỰA CHỌN CỦA MỌI NHÀ",
+        "en-US":"CHOICE FOR EVERY HOME",
+        },
+	"CONTENTS_DETAIL_CAPTION":{
+		"vi-VN":"Nảy mầm là sự phát triển của cây bắt đầu từ hạt và kết quả là hình thành cây con. HẠT GIỐNG rất quan trọng trong trồng trọt. Vì vậy, cần phải lựa chọn một cách chính xác. Giống tốt sẽ giúp tăng năng suất cây trồng và chất lượng sản phẩm tốt hơn.",
+		"en-US":"Germination is the development of a plant starting from a seed and resulting in the formation of a seedling. SEEDS are very important in farming. Therefore, it is necessary to choose correctly. Good varieties will help increase crop yield and better product quality."
+	},
+    "CONTENTS_BTN_CAPTION":{
+		"vi-VN":"Tìm hiểu thêm",
+		"en-US":"Learn more",
+		},
+    // HEADING - slide3
+	"CONTENTS_HEAD3":{
+		"vi-VN":"Chào mừng đến với",
+		"en-US":"Welcome to",
+		},
+    "CONTENTS_SLOGAN3":{
+        "vi-VN":"SỰ LỰA CHỌN CỦA MỌI NHÀ",
+        "en-US":"CHOICE FOR EVERY HOME",
+        },
+	"CONTENTS_DETAIL_TEXTEND":{
+		"vi-VN":"Sau một vụ mùa bội thu, đối với những loại đất có đặc tính xấu như chua, phèn, mặn, bạc màu… cần phải cải tạo để giảm bớt những đặc tính xấu đó để cây có thể sinh trưởng tốt hơn. Ngoài đất, và không thể không kể đến 4 yếu tố: Nước, Phân bón, Chăm sóc và Hạt giống.",
+		"en-US":"After a bountiful crop, for soils with bad characteristics such as sour, alum, salty, infertile, etc., it is necessary to improve to reduce those bad characteristics so that the plants can grow better. In addition to soil, there are 4 factors that cannot be ignored: Water, Fertilizer, Care and Seed."
+	},
+    "CONTENTS_BTN_TEXTEND":{
+		"vi-VN":"Khám phá thư viện",
+		"en-US":"Browse gallery",
+		},
+	//OUR STORY
+	"CONTENTS_OS_TITLE":{
+		"vi-VN":"Câu chuyện của chúng tôi",
+		"en-US":"Our Story",
+		},
+	"CONTENTS_OS_Target":{
+		"vi-VN":"đã ra đời với một sứ mệnh và tầm nhìn để tạo nên sự khác biệt về mặt tích cực trên thế giới.",
+		"en-US":"opened its doors with a mission and vision of making a positive difference in the world."
+	},
+	"CONTENTS_OS_DETAIL":{
+		"vi-VN":"Ngày 25/8/2011, <b class='display-7'> Ecofarm </b> khai trương hoạt động với sứ mệnh và tầm nhìn tạo ra sự khác biệt trên từng bài viết, hoàn thiện từng ngày và mang đến điều đặc biệt cho khách hàng. 10 năm đối với chúng tôi - đó là một hành trình, không phải là một cuộc chạy đua để xây dựng di sản và danh tiếng của những người giỏi nhất, mang đến những điều tốt nhất và tạo ra một <b class='display-7'> Ecofarm </b>. Chúng tôi tự hào về mỗi ngày. chúng tôi rất khiêm tốn và biết ơn về cơ hội mở cửa mỗi ngày, để tạo ra những trải nghiệm và kết quả đặc biệt mang lại cho chúng tôi những phản hồi tốt từ khách hàng.",
+		"en-US":"On August 25-2011, <b class='display-7'> Ecofarm </b> opened its operation with the mission and vision to make a difference on each article, perfect each day and bring something special to customers. 10 years for us - that is a journey, not a race to build a legacy and reputation of the best, bring out the best and create an <b class='display-7'>EcoFarm.</b>. We are proud of every day. we are humbled and grateful for the opportunity to open our doors every day, to create exceptional experiences and results that bring us good feedback from our customers."
+	},
+	"CONTENTS_OS_BTN":{
+		"vi-VN":"Xem thêm",
+		"en-US":"READ MORE"
+	},
+    // PRODUCTS WE DISTRIBUTION
+    "CONTENTS_PWD_TITLE":{
+		"vi-VN":"SẢN PHẨM CHÚNG TÔI PHÂN PHỐI",
+		"en-US":"PRODUCTS WE DISTRIBUTION",
+		},
+    "CONTENTS_PWD_TITLE2":{
+        "vi-VN":"Chúng tôi chuyên phân phối sỉ và lẻ các sản phẩm nhập khẩu từ <b class='text-muted1'>Hàn Quốc, Pháp, Hồng Kông, Nhật Bản </b>... như hạt giống, phân bón, nông cụ.",
+        "en-US":"We specialize in wholesale and retail distribution of imported products from <b class='text-muted1'>Korea, France, Hong Kong, Japan</b> ... such as seeds, fertilizers, agricultural tools.",
+        },
+    //PRODUCTS WE DISTRIBUTION - SEEDS
+    "CONTENTS_PWD_SEEDS":{
+        "vi-VN":"Hạt Giống",
+        "en-US":"Seeds",
+        },
+    "CONTENTS_PWD_SEEDS_DETAILS":{
+        "vi-VN":"Không giống như các sản phẩm thực vật khác, hạt giống được sử dụng làm nguyên liệu để nhân giống. Đảm bảo hạt giống khỏe mạnh là điều kiện tiên quyết để cây khỏe mạnh.",
+        "en-US":"Unlike other plant products, seeds are used as raw materials for propagation. Ensuring healthy seeds is a prerequisite for healthy plants. Quality of its seed for the purpose of selling it to other farmers and gardeners.",
+        },
+    "CONTENTS_SEEDS_BTN":{
+        "vi-VN":"Xem chi tiết »",
+        "en-US":"View details »",
+        },
+    //PRODUCTS WE DISTRIBUTION - FERTILIZER
+    "CONTENTS_PWD_FERTILIZER":{
+        "vi-VN":"Phân Bón",
+        "en-US":"Fertilizer",
+        },
+    "CONTENTS_PWD_FERTILIZER_DETAILS":{
+        "vi-VN":"Phân bón là nguồn cung cấp chất dinh dưỡng chính cho cây trồng. Bổ sung các chất cần thiết cho đất và môi trường nước, để cây hút đủ dinh dưỡng trong đất, hoặc bón trực tiếp qua lá cho cây.",
+        "en-US":"Fertilizers are the main source of nutrients for plants. Supplement the necessary substances for the soil and water environment, so that the plants can absorb enough nutrients in the soil, or directly fertilize through the leaves of the plants.",
+        },
+    "CONTENTS_FER_BTN":{
+        "vi-VN":"Xem chi tiết »",
+        "en-US":"View details »",
+        },
+    //PRODUCTS WE DISTRIBUTION - TOOLS
+    "CONTENTS_PWD_TOOLS":{
+        "vi-VN":"Nông Cụ",
+        "en-US":"Agricultural Tools",
+        },
+    "CONTENTS_PWD_TOOLS_DETAILS":{
+        "vi-VN":"Nông cụ - vật dụng (công cụ, đồ dùng) dùng để sản xuất nông nghiệp theo phương pháp thủ công, truyền thống. Nông cụ phục vụ sản xuất nông nghiệp rất đa dạng và phong phú như cuốc, xẻng, ...",
+        "en-US":"Agricultural tools - items (tools, utensils) used for agricultural production by manual and traditional methods. Agricultural tools for agricultural production are very diverse and rich such as hoe, shovel...",
+        },
+    "CONTENTS_TOOL_BTN":{
+        "vi-VN":"Xem chi tiết »",
+        "en-US":"View details »",
+        },
+    // Some of the best selling Products
+    "CONTENTS_BSP_HEAD":{
+		"vi-VN":"Một Số Sản Phẩm Bán Chạy Nhất!",
+		"en-US":"Some of the best selling Products",
+		},
+    "CONTENTS_BSP_DETAILS":{
+		"vi-VN":"là nơi tập trung đa dạng các loại mặt hàng,… nhằm đáp ứng tốt nhất nhu cầu của người tiêu dùng hiện nay.",
+		"en-US":"is home to a variety of items,... in order to best meet the needs of today's consumers.",
+		},
+    //French Red Marigold Seeds
+    "CONTENTS_BSP_MARIGOLD_HEAD":{
+		"vi-VN":"Hạt Giống Hoa Cúc Vạn Thọ Đỏ Pháp",
+		"en-US":"French Red Marigold Seeds",
+		},
+    "CONTENTS_BSP_MARIGOLD_DETAILS":{
+		"vi-VN":"Cúc vạn thọ Pháp là loài hoa dễ trồng lại nhanh ra hoa được nhiều người yêu thích và không thể vắng bóng trong dịp Tết cổ truyền. - Dạng ngắn, lùn, hoa to, trồng trang trí rất đẹp. <br> - Hoa nở 45-50 ngày sau khi gieo từ hạt. <br> - Hoa nhiều, nở liên tục và lâu tàn. <br> - Là loài hoa dễ tính, thích hợp với nhiều loại đất và thời tiết khác nhau.",
+		"en-US":"French marigold is an easy-to-replant, fast-flowering flower that is loved by many people and cannot be absent during the traditional Tet holiday. <br>  - Short form, dwarf, big flower, planted very nice decoration. <br> - Flowers bloom 45-50 days after sowing from seed. <br> - Flowers are many, bloom continuously and last a long time. <br> - An easy-going flower, suitable for many different types of soil and weather.",
+		},
+    "CONTENTS_BSP_BTN11":{
+		"vi-VN":"ĐẶT HÀNG NGAY",
+		"en-US":"Order NOW",
+		},
+    // Fertilizer NPK
+    "CONTENTS_BSP_NPK_HEAD":{
+		"vi-VN":"Phân Bón NPK",
+		"en-US":"Fertilizer NPK",
+		},
+    "CONTENTS_BSP_NPK_DETAILS":{
+		"vi-VN":"Thích hợp cho thời kỳ sinh trưởng và phát triển của tán lá, thích hợp bón cho cây ăn quả và cây lâu năm. <br> - Chất đạm (N): 10%. Phốt pho (P2O5): 6%. Kali (K2O): 4%. Và các nguyên tố vi lượng khác <br> - Sử dụng dễ dàng, trong quá trình sử dụng không phải bổ sung thêm đạm và kali trong quá trình sử dụng.<br> - Giúp cứng cây, chống chịu sâu bệnh, từ đó tăng năng suất, chất lượng nông sản. Là loại phân tan chậm, hạn chế rửa trôi và bay hơi.",
+		"en-US":"Suitable for the period of growth and development of foliage.Suitable for fertilizing fruit trees and perennials. <br> - Protein (N): 10%. Phosphorus (P2O5): 6%. Potassium (K2O): 4%. And other trace elements <br> - Easy to use, during use do not have to add protein and potassium during use. <br> - Helps to harden trees, resist pests and diseases, thereby increasing productivity and quality of agricultural products. It is a slow-dissolving fertilizer, limiting leaching and evaporation.",
+		},
+    "CONTENTS_BSP_BTN22":{
+		"vi-VN":"ĐẶT HÀNG NGAY",
+		"en-US":"Order NOW",
+		},
+    // Box Of 10 Garden Tools
+    "CONTENTS_BSP_10GARDENTOOLS_HEAD":{
+		"vi-VN":"Hộp 10 Dụng Cụ Làm Vườn (Màu Xanh)",
+		"en-US":"Box Of 10 Garden Tools (Green)",
+		},
+    "CONTENTS_BSP_10GARDENTOOLS_DETAILS":{
+		"vi-VN":"Với hộp đựng dụng cụ này chắc chắn bạn sẽ không cần phải mua rời từng món, hơn nữa việc để các dụng cụ vào chung một hộp sẽ bảo quản tốt hơn, thuận tiện mang đi bất cứ đâu một cách dễ dàng. <br> - Hộp đựng sản phẩm gồm 10 món Xẻng, cào, bay, kéo, bình xịt, ... được đặt gọn gàng trong chiếc hộp sạch sẽ xinh xắn với các dụng cụ được làm chủ yếu từ thép sơn tĩnh điện chống gỉ sét tăng độ bền. , tay cầm bọc nhựa có hoa văn chống trơn trượt.",
+		"en-US":"With this tool box, it is certain that you will not need to buy each item separately, moreover, putting the tools in the same box will preserve better and be convenient to carry anywhere easily. <br> - The product box with 10 items Shovel, rake, trowel, scissors, sprayer, ... are neatly placed in a lovely clean box with tools made mainly from steel with electrostatic paint to prevent rust to increase durability. , plastic coated handle with anti-slip pattern.",
+		},
+    "CONTENTS_BSP_BTN33":{
+		"vi-VN":"ĐẶT HÀNG NGAY",
+		"en-US":"Order NOW",
+		},
+    // Our client Say
+    "CONTENTS_OCS_TITLE":{
+		"vi-VN":"Phản Hồi Từ Khách Hàng!",
+		"en-US":"Our Client Say!",
+		},
+    // David
+    "CONTENTS_OCS_DAVID":{
+		"vi-VN":"Tuyệt vời, giao hàng nhanh, đã mua nhiều lần nên mọi người cứ yên tâm, lần sau sẽ ủng hộ.",
+		"en-US":"Great, fast delivery, have bought many times so everyone can rest assured, will support next time.",
+		},
+    // Lalisa
+    "CONTENTS_OCS_Lalisa":{
+		"vi-VN":"Shop bán nhiều loại, tuy ở xa nhưng giao hàng rất nhanh, bộ dụng cụ làm vườn đầy đủ giúp mình không phải mua riêng, rất tiện lợi.",
+		"en-US":"The shop sells many types, although far away, but the delivery is very fast, the full set of garden tools helps me not to buy separately, very convenient.",
+		}, 
+    //Justin
+    "CONTENTS_OCS_Justin":{
+		"vi-VN":"Quá ngạc nhiên, hạt giống phát triển rất tốt, giúp vụ mùa bội thu, cảm ơn EcoFarm!",
+		"en-US":"So surprised, the seeds grow very well, helping the crop to be bountiful, thank you EcoFarm!",
+		},
+    // Contact Us
+    "CONTENTS_CTS_BTN":{
+		"vi-VN":"Liên hệ với chúng tôi",
+		"en-US":"Contact Us",
+		},
+    // Our Blogs
+    "CONTENTS_OB_TITLE":{
+		"vi-VN":"Blogs Chia sẻ",
+		"en-US":"Our Blogs",
+		},
+	"CONTENTS_OB_LINK":{
+		"vi-VN":"Xem Thêm",
+		"en-US":"READ MORE",
+		},
+	"CONTENTS_OB_LINK2":{
+		"vi-VN":"Xem Thêm",
+		"en-US":"READ MORE",
+		},
+	"CONTENTS_OB_LINK3":{
+		"vi-VN":"Xem Thêm",
+		"en-US":"READ MORE",
+		},
+    // Rice cultivation techniques
+    "CONTENTS_OB_RCT_TITLE":{
+		"vi-VN":"Kỹ thuật trồng lúa",
+		"en-US":"Rice cultivation techniques",
+		},
+    "CONTENTS_OB_RCT_DETAILS0":{
+		"vi-VN":"Quy trình trồng lúa bao gồm: chọn giống, làm đất, gieo sạ, bón phân, quản lý nước, phòng trừ sâu bệnh và thu hoạch.",
+		"en-US":"The rice growing process includes: seed selection, soil preparation, sowing, fertilization, water management, pest and disease control, and harvesting.",
+		},
+	"CONTENTS_OB_RCT_DETAILS":{
+		"vi-VN":" <li class='feature-item'> Chọn giống lúa: Giống là yếu tố đầu tiên quyết định năng suất ... </li> <li class='feature-item'> Gieo: Có thể gieo cấy, gieo thẳng. Ngày nay, khoa học kỹ thuật ngày càng tiên tiến ... </li> <li class='feature-item'> Quá trình bón phân cho cây </li> <li class='feature-item'> Nước tưới: Nước tưới có vai trò rất quan trọng đối với sự sinh trưởng của cây lúa ... </li> ",
+		"en-US":" <li class='feature-item'> Choose rice varieties: The variety is the first factor that determines the yield... </li> <li class='feature-item'> Sowing: You can sow transplant, sowing straight. Today, science and technology are more and more advanced... </li> <li class='feature-item'> Fertilization process... </li> <li class='feature-item'> Irrigation water: Irrigation water plays a very important role for the growth of rice plants... </li>",
+		},		
+    // Are you watering properly?
+    "CONTENTS_OB_WP_TITLE":{
+		"vi-VN":"Bạn đã nước nước đúng cách?",
+		"en-US":"Are you watering properly?",
+		},
+	"CONTENTS_OB_WP_DETAILS0":{
+		"vi-VN":"Vấn đề tưới nước tưởng chừng như đơn giản nhưng lại là nguyên nhân chính khiến cây của bạn bị chết, nhất là trong những ngày này. Bây giờ <b class='display-7'> Ecofarm </b> sẽ bật mí cho bạn một bí quyết nhỏ trong việc tưới nước và chăm sóc cây ...",
+		"en-US":"The problem of watering seems to be simple, but it is the main cause of your plant's death, especially these days. Now <b class='display-7'> Ecofarm </b> will reveal to you a little secret in watering and taking care of trees...",
+		},
+	"CONTENTS_OB_WP_DETAILS":{
+		"vi-VN":"<li class='feature-item'> Bạn dùng nước gì để tưới? ... </li> <li class='feature-item'> Tưới nước bao nhiêu là đủ? ... </li> <li class='feature-item'> Tưới nước bao nhiêu là đủ? ... </li> <li class='feature-item'> Nên tưới cây ở vị trí nào? ... </li> <li class='feature-item'> Có nên lắp đặt hệ thống tưới tự động không? ... </li>",
+		"en-US":" <li class='feature-item'> What water do you use for irrigation?... </li> <li class='feature-item'> How much watering is enough?... </li> <li class='feature-item'> How often or at what time should you water?... </li> <li class='feature-item'> Which position of the plant should be watered?... </li> <li class='feature-item'> Should I install an automatic irrigation system?... </li>",
+		},
+    // Techniques for growing French red marigolds
+    "CONTENTS_OB_TG_TITLE":{
+		"vi-VN":"Kỹ thuật trồng cúc vạn thọ đỏ Pháp",
+		"en-US":"Techniques for growing French red marigolds",
+		},
+    "CONTENTS_OB_TG_DETAILS0":{
+		"vi-VN":"Đối với người trồng hoa Tết, việc lựa chọn giống hoa phù hợp với thị trường tiêu thụ luôn là mối quan tâm hàng đầu. HOA CÚC PHAP là một loài hoa như vậy: đẹp, lớn và hiệu quả kinh tế cao, được nhân dân ta ưa chuộng, nhất là trong dịp Tết.",
+		"en-US":"For Tet flower growers, choosing flower varieties suitable for the consumption market is always a top concern. THE PHAP CUCCULTURE is such a flower: beautiful, large and high economic efficiency, popular with our people, especially during Tet.",
+		},
+    "CONTENTS_OB_TG_DETAILS":{
+		"vi-VN":" <li class='feature-item'> Gieo: Nên gieo hạt từ ngày 20 đến 30 tháng 10 âm lịch để cây ra hoa kịp Tết ... </li> <li class='feature-item'> Chăm sóc cây trồng </li> <li class='feature-item'> Phân bón: Khi cây đã bắt đầu ra nụ, bón phân sẽ giúp màu hoa tươi và đẹp lâu hơn ... </li>",
+		"en-US":" <li class='feature-item'> Sowing: Seeds should be sown from October 20 to 30 of the lunar calendar to flower in time for Tet... </li> <li class='feature-item'> Care... </li> <li class='feature-item'> Fertilizer: When the plant has begun to bud, the fertilizer will help keep the flower color fresh and beautiful longer... </li>",
+		},
+    // Our Team
+    "CONTENTS_OT_TITLE":{
+		"vi-VN":"Nhóm của chúng tôi",
+		"en-US":"Our team",
+		},
+    "CONTENTS_OT_DETAIL":{
+		"vi-VN":"là đứa con tinh thần của chúng tôi trong suốt thời gian qua, để có được một <b class='display-7'>EcoFarm</b> hoàn chỉnh chúng tôi đã phải trải qua rất nhiều thử thách và khó khăn để có được vị trí như ngày hôm nay. Cảm ơn mọi người...",
+		"en-US":"is one of our children during the past time, to have a complete <b class='display-7'>EcoFarm</b> we had to go through many challenges and difficulties to get where we are today. Thanks everyone...",
+		},
+    "CONTENTS_OT_1":{
+		"vi-VN":"Huỳnh Quý Khang",
+		"en-US":"Quy Khang Huynh",
+		},
+    "CONTENTS_OT_11":{
+		"vi-VN":"CEO - Giám đốc điều hành",
+		"en-US":"CEO - Founder",
+		},
+    "CONTENTS_OT_2":{
+		"vi-VN":"Ngô Huỳnh Ngân",
+		"en-US":"Huynh Ngan Ngo",
+		},
+    "CONTENTS_OT_22":{
+		"vi-VN":"Trưởng nhóm kỹ thuật",
+		"en-US":"Tech - Leader",
+		},
+    "CONTENTS_OT_3":{
+		"vi-VN":"Lê Bá Tùng",
+		"en-US":"Ba Tung Le",
+		},
+    "CONTENTS_OT_33":{
+		"vi-VN":"Quản lý sản phẩm",
+		"en-US":"Products Manager",
+		},
+    "CONTENTS_OT_4":{
+		"vi-VN":"Võ Văn Hùng",
+		"en-US":"Van Hung Vo",
+		},
+    "CONTENTS_OT_44":{
+		"vi-VN":"Nhà phát triển",
+		"en-US":"Developer",
+		},
+    "CONTENTS_OT_5":{
+		"vi-VN":"Nguyễn Kiều Hân",
+		"en-US":"Kieu Han Nguyen",
+		},
+    "CONTENTS_OT_55":{
+		"vi-VN":"Nhà thiết kế",
+		"en-US":"Editor",
+		},
+};
